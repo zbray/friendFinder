a35ed21e801a7411981f5c3a288e4b1e570daf5d
Cold Zebra <zachdbray@gmail.com>
2019-03-07 21:08:24 -0600

    changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,5 @@
-//Dependencies
-var express = require('express');
-var path = require('path');
+//Empty array to store survey entries
 
-//Initialize Server
-var app = express();
-var PORT = process.env.PORT || 8080;
+var friendsArray = [];
 
-
-$('#thanksModal')
-
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+moedul.exports = friendArray;
