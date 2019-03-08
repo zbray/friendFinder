fdb1af72192ab99565337e02f9642541b831443f
Cold Zebra <zachdbray@gmail.com>
2019-03-07 20:33:07 -0600

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+//Dependencies
+var express = require('express');
+var path = require('path');
+
+//Initialize Server
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+$('#thanksModal')
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
