d3da2a43a588a942358aff655700bf71a3c43692
Cold Zebra <zachdbray@gmail.com>
2019-03-10 16:32:33 -0500

    changes. begun match function

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,32 @@
 
 var friendsArray = [{
   "name": "Monica",
-  "photo": "https://vignette.wikia.nocookie.net/friends/images/c/c8/Fat_Monica.jpg/revision/latest?cb=20110723171344",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Courteney_Cox_1995.jpg/260px-Courteney_Cox_1995.jpg",
   "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-}];
+}, {
+  "name": "Rachel",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/JenniferAniston08TIFF.jpg/260px-JenniferAniston08TIFF.jpg",
+  "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+  "name": "Phoebe",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Lisa_Kudrow_3.jpg/260px-Lisa_Kudrow_3.jpg",
+  "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+  "name": "Joey",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Matt_Le_Blanc.jpg/260px-Matt_Le_Blanc.jpg",
+  "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+  "name": "Chandler",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Matthew_Perry_1995_%28cropped%29.jpg/260px-Matthew_Perry_1995_%28cropped%29.jpg",
+  "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+  "name": "Ross",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/David_Schwimmer_2005_Madagascar.jpg/260px-David_Schwimmer_2005_Madagascar.jpg",
+  "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,26 +4,47 @@ var friendsArray = require("../data/friends.js");
 module.exports = function (app) {
   app.get("/api/friends", function (req, res) {
     res.json(friendsArray);
-
   });
+
   app.post("/api/friends", function (req, res) {
-    console.log("req: " + req);
-    console.log("friendsArray: " + friendsArray)
     //Store latest Survey Results
-    var user = {
-      name: req.body.name,
-      photo: req.body.photo,
-      scores: JSON.parse(req.body.scores)
+    var user = req.body;
+    console.log("12a req.body: " + user);
+    var userName = user.name;
+    console.log("14a req.body.name: " + userName);
+    console.log("15a req.body.photo: " + user.photo);
+    var userScores = user.scores;
+    console.log("17a req.body.scores: " + userScores);
+    console.log("178a: JSONuser" + JSON.stringify(user));
+
+
+    var bestFriend = {
+      bfName: "",
+      bfPhoto: "",
+      bfDifference: 10
+    };
+
+    var scoreDiff = 0;
+
+    //Loop through all scores and friends
+    for (var i = 0; i < friendsArray.length; i++) {
+      console.log(friendsArray[i].name);
+
+      for (var j = 0; j < friendsArray[i].scores.length; j++) {
+        scoreDiff += Math.abs(parseInt(userScores[j]) - parseInt(friendsArray[i].scores[j]));
+        // console.log("30a friendsArray[i].scores[j]: " + friendsArray[i].scores[j]);
+        // console.log("31a userScores[j]: " + userScores[j]);
+      }
     }
-    console.log("user: " + user)
+
+    // console.log("Best Friend Name: " + bestFriend.name);
+    // console.log("Best Friend Photo: " + bestFriend.photo);
 
     //Push to Array
-    friendsArray.push(user);
-    console.log("22name" + req.body.name)
-    console.log("23photo" + req.body.photo)
-    console.log("24scores" + req.body.scores)
     friendsArray.push(req.body);
-    //Find Match
 
   });
 }
+
+// matchName
+// matchPhoto
