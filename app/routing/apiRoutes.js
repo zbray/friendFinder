d9bc829215c83e0063d743a6921c826fd263c7d9
Cold Zebra <zachdbray@gmail.com>
2019-03-10 17:12:54 -0500

    fixed issue with NaN - differences now readable

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,26 +23,42 @@ module.exports = function (app) {
       bfPhoto: "",
       bfDifference: 10
     };
+    var totalDiff = 0;
 
-    var scoreDiff = 0;
 
-    //Loop through all scores and friends
+    // Loop through all scores and friends
     for (var i = 0; i < friendsArray.length; i++) {
-      console.log(friendsArray[i].name);
+      console.log("31a names: "+friendsArray[i].name);
+      var totalDiff = 100;
 
+      //Loop through all the scores of all the stored friends
       for (var j = 0; j < friendsArray[i].scores.length; j++) {
-        scoreDiff += Math.abs(parseInt(userScores[j]) - parseInt(friendsArray[i].scores[j]));
+        console.log("36a userScores[j]: " + userScores[j])
+        console.log("37a friendsArray[i].scores[j]:" + friendsArray[i].scores[j]);
+        console.log("38a userScores " + userScores);
+        totalDiff += Math.abs(userScores[j] - friendsArray[i].scores[j]);
+
         // console.log("30a friendsArray[i].scores[j]: " + friendsArray[i].scores[j]);
         // console.log("31a userScores[j]: " + userScores[j]);
+        console.log("43a totalDiff: " + totalDiff)
+        // totalDiff += difference;
+        // console.log(totalDiff)
+
+
+
+
       }
     }
+
+
+
+
 
     // console.log("Best Friend Name: " + bestFriend.name);
     // console.log("Best Friend Photo: " + bestFriend.photo);
 
     //Push to Array
     friendsArray.push(req.body);
-
   });
 }
 
